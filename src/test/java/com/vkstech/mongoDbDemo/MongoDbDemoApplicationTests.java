f24ef8b16a94e487a0a897137dc27026c08de9f5
Vipul Kumar Singh <vipul24.9669@gmail.com>
2020-05-25 14:51:04 +0530

    Initial Commit

---
+++ src/test/java/com/vkstech/mongoDbDemo/MongoDbDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vkstech.mongoDbDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongoDbDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
